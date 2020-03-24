bd539a29df6d3cd320e05ff4f96ad65c067b1b0b
xiuwen233 <32030117+xiuwen233@users.noreply.github.com>
2020-03-24 11:28:17 +0800

    Update findcouter.cpp

--- opencv/findcouter.cpp
+++ opencv/findcouter.cpp
@@ -48,3 +48,34 @@ vector<vector<Point>> contours;
 				is_findcouter = true;
 			}
 		}
+
+
+findContours(getrch, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point());
+		vector<Rect> boundRect(contours.size());
+		for (int i = 0; i < contours.size(); i++)
+		{
+			rect = minAreaRect((contours[i]));
+			
+			cvBoxPoints(rect, Corners);
+			boundRect[i] = boundingRect(Mat(contours[i]));
+			if (rect.size.width  * rect.size.height > 10000 &&
+				rect.size.width > 100 &&
+				rect.size.height > 100) {
+
+				line(frame, Point((int)Corners[0].x, (int)Corners[0].y), Point((int)Corners[1].x, (int)Corners[1].y), Scalar(255, 0, 255), 2, 8, 0);
+				line(frame, Point((int)Corners[1].x, (int)Corners[1].y), Point((int)Corners[2].x, (int)Corners[2].y), Scalar(255, 0, 255), 2, 8, 0);
+				line(frame, Point((int)Corners[2].x, (int)Corners[2].y), Point((int)Corners[3].x, (int)Corners[3].y), Scalar(255, 0, 255), 2, 8, 0);
+				line(frame, Point((int)Corners[0].x, (int)Corners[0].y), Point((int)Corners[3].x, (int)Corners[3].y), Scalar(255, 0, 255), 2, 8, 0);
+
+				for (int j = 0; j < 4; j++)
+				{
+					corner_point[j].x = (int)Corners[j].x;
+					corner_point[j].y = (int)Corners[j].y;
+				}
+
+				rectangle(frame, Point(boundRect[i].x , boundRect[i].y ),
+					Point(boundRect[i].x + boundRect[i].width*0.9 , boundRect[i].y + boundRect[i].height*0.9), Scalar(255), 2, 8);
+				ResultRect = SmallGetTheRectRangle();
+				findcouter_number++;
+			}
+		}

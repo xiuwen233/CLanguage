bd04083ee27c3c2392a1b419be50307ad1ae0e9b
xiuwen233 <32030117+xiuwen233@users.noreply.github.com>
2020-03-24 12:07:19 +0800

    Update DMtxknow.cpp

--- opencv/DMtxknow.cpp
+++ opencv/DMtxknow.cpp
@@ -22,3 +22,32 @@ DmtxMessage *msg;
 				else {
 					//is_detectcode = false;
 				}
+
+{
+	DmtxMessage *msg;
+	DmtxRegion *reg;
+	Mat src = imread("E://photo_opencv/boxTest/555666.jpg");
+	if (!src.data) {
+		cout << "Load image failed!" << endl;
+		return 0;
+	}
+	DmtxImage *img;
+	img = dmtxImageCreate(src.data, src.cols, src.rows, DmtxPack24bppRGB);
+	DmtxDecode *dec = dmtxDecodeCreate(img, 1);
+	reg = dmtxRegionFindNext(dec, NULL);
+	if (reg != NULL) {
+		msg = dmtxDecodeMatrixRegion(dec, reg, DmtxUndefined);
+		if (msg != NULL) {
+			//cout << msg->output << endl;
+			printf("msg->output = %s \n", msg->output );
+		    printf(" msg->array = %x\n",msg->array);
+			printf("msg->code = %x \n",msg->code);
+			dmtxMessageDestroy(&msg);
+		}
+		dmtxRegionDestroy(&reg);
+	}
+	dmtxDecodeDestroy(&dec);
+	dmtxImageDestroy(&img);
+	getchar();
+	return 0;
+}
